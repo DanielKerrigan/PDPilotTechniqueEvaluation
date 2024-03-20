4fab6842eeb69416201bb6a43eb1c56c29902296
DanielKerrigan <d.kerrigan111@gmail.com>
2024-03-20 02:08:38 -0400

    dont have icon for unlabeled plots

--- filtering/labeling/src/routes/Sidebar.svelte
+++ filtering/labeling/src/routes/Sidebar.svelte
@@ -48,7 +48,7 @@
 							stroke-linejoin="round"
 							d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
 						/>
-					{:else}
+					{:else if curve.shape === 'mixed'}
 						<path
 							stroke-linecap="round"
 							stroke-linejoin="round"
