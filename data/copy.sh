3c2ccc72d261aa31e8bb472380521cce876aef6a
Daniel Kerrigan <d.kerrigan111@gmail.com>
2024-12-02 16:08:52 -0500

    update job scripts

--- data/copy.sh
+++ data/copy.sh
@@ -1,3 +1,3 @@
 # Usage:
 # ./copy.sh [small|big]
-rsync -av kerrigan.d@xfer.discovery.neu.edu:/scratch/kerrigan.d/pdpilot/"$1" ./results
+rsync -av kerrigan.d@xfer.discovery.neu.edu:/work/vis/users/kerrigan.d/PDPilot/PDPilotTechniqueEvaluation/data/results/"$1" ./results
--- data/job-big.sh
+++ data/job-big.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --partition=short
 #SBATCH --job-name=pdpilot-big-all
-#SBATCH --output=/scratch/kerrigan.d/pdpilot/big/output/%A_%a.out
-#SBATCH --error=/scratch/kerrigan.d/pdpilot/big/output/%A_%a.err
+#SBATCH --output=results/big/output/%A_%a.out
+#SBATCH --error=results/big/output/%A_%a.err
 #SBATCH --array=0-36
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
-source /home/kerrigan.d/miniconda3/bin/activate
+source /work/vis/users/kerrigan.d/miniforge3/bin/activate
 conda activate pdpilot-eval
 
-srun python data_models_plots.py -g big -i $SLURM_ARRAY_TASK_ID -o /scratch/kerrigan.d/pdpilot/big
+srun python data_models_plots.py -g big -i $SLURM_ARRAY_TASK_ID -o results/big
